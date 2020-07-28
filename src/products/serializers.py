6dbb018507ac37ae9362dbbd892c5bdc07709c12
Yong En Lee <yongenlee@Yongs-MacBook-Pro-2.local>
2020-07-29 01:05:56 +0800

    minor changes to p. serializers

--- src/products/serializers.py
+++ src/products/serializers.py
@@ -3,7 +3,9 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+  id = serializers.ReadOnlyField()
   class Meta:
     model = Product
-    fields = ('title', 'desc', 'price', 'summary', 'featured')
+    fields = ('id', 'title', 'desc', 'price', 'summary', 'featured')
+    read_only_fields = ['id']
 
